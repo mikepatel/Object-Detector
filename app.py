3277505da8fe991636ad00498c4a1ed58c8e67dd
Michael Patel <michael.patel94@gmail.com>
2020-06-12 12:00:19 -0400

    Opening webcam

--- app.py
+++ app.py
@@ -12,12 +12,30 @@ File description:
 ################################################################################
 # Imports
 import os
-
+import numpy as np
+import cv2
 import tensorflow as tf
 
 
 ################################################################################
 # Main
 if __name__ == "__main__":
+    # open webcam and capture video
+    capture = cv2.VideoCapture(0)  # 0 = first camera
+
+    while True:
+        # capture frame by frame
+        ret, frame = capture.read()
+
+        # display resulting frame
+        cv2.imshow("", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    # release capture
+    capture.release()
+    cv2.destroyAllWindows()
+
     # use pretrained model
 
